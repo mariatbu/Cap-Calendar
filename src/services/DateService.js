e842c9599129839aa296e6e63ee0dda8c7d69e75
Javier Quiñonero <javier.quinoner-gonzalez@capgemini.com>
2021-09-06 19:37:55 +0200

    DateService

--- src/services/DateService.js
+++ src/services/DateService.js
@@ -1,8 +1,27 @@
-export class DateService{
-    static getDayOfMonth(date){
+export class DateService {
+    static getDayOfMonth(date) {
+        function calendarMonth(fecha) {
+            let calendar = [];
+            getFirstDayCalendar(fecha);
+            let i = 1;
+            for (i = 0; i < 42; i++) {
+                calendar.push(new Date(fecha));
+                fecha.setDate(fecha.getDate() + 1);
+            }
+            return calendar;
+        }
+
+        function getFirstDayCalendar(fecha) {
+            fecha.setDate(1);
+            while (fecha.getDay() != 1) { fecha.setDate(fecha.getDate() - 1); }
+        }
 
     }
-    static addMonth(date, diff){
+
+    static addMonth(date, diff) {
+        date.setMonth(date.getMonth() + diff)
+        return DateService.getDayOfMonth(date);
+
     }
 
 }
