ec0bc1f787624b7f614da4a08526cde6a33aa3e9
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-07 08:21:41 +0200

    Timer Interface done with bind

--- src/services/TimerInterface.js
+++ src/services/TimerInterface.js
@@ -1,4 +1,4 @@
 export const timerInterface = {
-    setInterval:(handler, timeInterval)=>{this.setInterval(handler, timeInterval).bind(window.setInterval(handler, timeInterval))},
-    clearInterval:(intervalID)=>{this.clearInterval(intervalID).bind(window.clearInterval(intervalID))}
+    setInterval: window.setInterval.bind(window),
+    clearInterval: window.clearInterval.bind(window)
 }
