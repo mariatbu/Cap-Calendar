fe155d1c2566c1730922f14fbd534d5880cf2c3a
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-07 13:14:15 +0200

    monthdate structure done

---
+++ src/components/MonthDate.js
@@ -0,0 +1,16 @@
+import { FormatService } from "../services/FormatService.js"
+import pubSub from "../services/PubSub.js"
+import { CHANNELS } from "../services/Config.js"
+
+class MonthDate extends HTMLElement{
+
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+
+    }
+    disconnectedCallback(){
+        
+    }
+}
