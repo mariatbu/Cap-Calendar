7cf99a3124a32ee0f00e3e875296724717e28b70
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-08 11:26:01 +0200

    fixed border top

--- src/components/monthdate/monthdate.css.js
+++ src/components/monthdate/monthdate.css.js
@@ -4,6 +4,7 @@ sheet.replace(`
     color: rgb(230,230,230);
     background-color: rgb(70, 70, 70);
     font-family: sans-serif;
+    border-top: 0.05em solid darkgrey;
     }
     div:hover {
         color:white;
