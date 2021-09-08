bdf29f75e0a0600241cceb46be5aaa10c8fdfbba
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-08 11:29:06 +0200

    finished

--- src/components/monthdate/monthdate.css.js
+++ src/components/monthdate/monthdate.css.js
@@ -4,6 +4,8 @@ sheet.replace(`
     color: rgb(230,230,230);
     background-color: rgb(70, 70, 70);
     font-family: sans-serif;
+    border-top: 0.05em solid darkgrey;
+    padding: 1em 1em;
     }
     div:hover {
         color:white;
