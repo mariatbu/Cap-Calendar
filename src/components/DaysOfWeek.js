f02a1d3ed6645644720b55c35481f5fdb97813f2
Javier Pinas <javier.pinas-castellanos@capgemini.com>
2021-09-08 09:06:53 +0200

    dias de la semana

--- src/components/DaysOfWeek.js
+++ src/components/DaysOfWeek.js
@@ -1,1 +1,39 @@
 import {DAYSOFWEEK} from "../services/Config.js"
+
+const css = `:host{
+    display: grid;
+    gap: 8px;
+    grid-template-columns: repeat(7,25px);
+  } `
+
+class DaysOfWeeks extends HTMLElement{
+    constructor(){
+        super();
+        this.shadow = this.attachShadow({mode: "closed"});
+        this.text = null;
+    }
+
+    connectedCallback(){
+
+            let div= document.createElement('div');
+            div.appendChild(text);
+            const style = document.createElement("style");
+            style.textContent = css;
+            this._shadow.appendChild(style);
+            return div;
+
+
+        
+            
+    }
+
+    disconnectedCallback(){
+
+    }
+
+   
+}
+
+customElements.define('cap-days-of-weeks', DaysOfWeeks);
+
+export {DaysOfWeeks}
