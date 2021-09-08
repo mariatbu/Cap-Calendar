74d2e7f4e0185321848141013172a783942ee585
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-08 08:23:55 +0200

    Clock finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
-import "./services/TimerService.js"
-import "./components/SystemDate.js"
-import "./components/MonthDate.js"
+import "./services/TimerService.js";
+import "./components/SystemDate.js";
+import "./components/MonthDate.js";
+import "./components/Clock.js"
