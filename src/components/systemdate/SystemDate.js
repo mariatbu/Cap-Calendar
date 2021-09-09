ba12f5c4565c8deaf02bab12519bf05490493392
Javier Quiñonero <javier.quinoner-gonzalez@capgemini.com>
2021-09-09 11:13:25 +0200

    Calendar Button 2.0

--- src/components/systemdate/SystemDate.js
+++ src/components/systemdate/SystemDate.js
@@ -1,7 +1,8 @@
-import { FormatService } from "../services/FormatService.js";
-import { DateService } from "../services/DateService.js";
-import pubSub from "../services/PubSub.js";
-import { CHANNELS } from "../services/Config.js";
+import { FormatService } from "../../services/FormatService.js";
+import { DateService } from "../../services/DateService.js";
+import pubSub from "../../services/PubSub.js";
+import { CHANNELS } from "../../services/Config.js";
+import css from './systemdate.css.js';
 
 class SystemDate extends HTMLElement{
     constructor(){
@@ -9,18 +10,22 @@ class SystemDate extends HTMLElement{
         this._text = null;
         this.date = new Date();
     }
+    _formatDate() {
+        return FormatService.getSystemDate(this.date);
+    }
     connectedCallback(){
+        const shadow = this.attachShadow({mode:"closed"})
+        this._text = document.createTextNode(this._formatDate());
+        const div = document.createElement('div');
+        shadow.adoptedStyleSheets = [css];
+        div.appendChild(this._text);
+        shadow.appendChild(div);
         this._dispose = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
             this.date = date;
             if(!DateService.isToday(date, new Date())){
-                this._text.data = FormatService.getSystemDate(this.date);
+                text.data = this._formatDate();
             }
         } )
-        const shadow = this.attachShadow({mode:"closed"})
-        this._text = document.createTextNode(FormatService.getSystemDate(this.date));
-        const div = document.createElement('div');
-        div.appendChild(this._text);
-        shadow.appendChild(div);
     }
     disconnectedCallback(){
         this._dispose();
