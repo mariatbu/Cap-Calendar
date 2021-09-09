fbaeefaf6dfd7b2afb73b7ecd2b8645c2a90e567
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-09 09:46:35 +0200

    trying to fix errors on imports

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,35 +1,21 @@
 import { FormatService } from "../../services/FormatService.js";
-import pubSub from "../../services/PubSub.js";
-import { CHANNELS } from "../../services/Config.js";
+import { ComponentDateBase } from "../core/componentDateBase.js";
 import css from './clock.css.js';
 
-class Clock extends HTMLElement {
+class Clock extends ComponentDateBase {
  
-    constructor() {
-        super();
-        this.date = new Date();
-    }
- 
-    _formatDate() {
+    _formatDate(){
         return FormatService.getTimer(this.date);
     }
- 
-    connectedCallback() {
-        const shadow = this.attachShadow({ mode: "open" })
-        const div = document.createElement("div");
-        const text = document.createTextNode(this._formatDate());
-        div.appendChild(text);
-        shadow.appendChild(div);
-        shadow.adoptedStyleSheets = [css];
-        this._dispose = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
-                this.date = date;
-                text.data = this._formatDate();
-        });
+
+    _changeDate(value){
+        return true;
     }
- 
-    disconnectedCallback() {
-        this._dispose();
+
+    _getStyle(){
+        return this._shadow.adoptedStyleSheets = [css];
     }
+
 }
 window.customElements.define("cap-clock", Clock)
 export {Clock}
