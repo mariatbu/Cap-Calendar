ce084636c510a6ec039bafff397832d026c4889c
Maria Turpin <maria.turpin-buendia@capgemini.com>
2021-09-10 12:13:28 +0200

    event day added

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import "./services/TimerService.js";
 import "./components/systemdate/SystemDate.js";
 import "./components/monthdate/MonthDate.js";
 import "./components/clock/Clock.js"
-<<<<<<< HEAD
 import "./components/daysofweek/daysofweek.js"
 import "./components/core/componentDateBase.js"
 import "./services/FormatService.js"
-=======
 import "./components/buttons/CalendarButton.js"
->>>>>>> feature_CalendarButton
+import "./components/eventdate/eventdate.js"
