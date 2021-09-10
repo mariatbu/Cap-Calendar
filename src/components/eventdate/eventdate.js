4d8a2a1acbe85fa30b761cb13d9e771c4986ad2e
Javier Pinas <javier.pinas-castellanos@capgemini.com>
2021-09-10 11:59:44 +0200

    eventday middle finished

--- src/components/eventdate/eventdate.js
+++ src/components/eventdate/eventdate.js
@@ -2,7 +2,16 @@ import { FormatService } from "../../services/FormatService.js";
 import { ComponentDateBase } from "../core/componentDateBase.js";
 import { CHANNELS } from "../../services/Config.js";
 
-class EventDate extends ComponentDateBase{
+export class EventDate extends ComponentDateBase{
+
+    constructor(){
+        super();
+    }
+
+    _setStyle(){
+        return document.createElement("style");
+    }
+
 
     connectedCallback(){
         this._subscribe(CHANNELS.CHANGESELECTEDATE);
@@ -18,4 +27,4 @@ class EventDate extends ComponentDateBase{
 
 }
 
-window.customElements.define("cap-event-date", EventDate);
+customElements.define("cap-event-date", EventDate);
