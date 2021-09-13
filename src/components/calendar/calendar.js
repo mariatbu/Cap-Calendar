329cfc5d9e83fa1de2083d82d922df5874d9d5c9
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-13 18:36:31 +0200

    component calendar

---
+++ src/components/calendar/calendar.js
@@ -0,0 +1,31 @@
+export class Calendar extends HTMLElement{
+    constructor(){
+        super();
+        this._shadow = this.attachShadow({mode: "open"});
+        this._create();
+    }
+    _create(){
+        let clock = document.createElement("cap-clock");
+        this._shadow.appendChild(clock);
+        let sysDate = document.createElement("cap-system-date");
+        this._shadow.appendChild(sysDate);
+        let div = document.createElement("div");
+        div.setAttribute("style", "display:flex;border-top: 0.05em solid darkgrey; background-color: rgb(70,70,70);")
+        let monthDate = document.createElement("cap-month-date");
+        div.appendChild(monthDate);
+        let calendarDownButton = document.createElement("cap-button");
+        calendarDownButton.setAttribute("action", "down");
+        div.appendChild(calendarDownButton);
+        let calendarUpButton = document.createElement("cap-button");
+        calendarUpButton.setAttribute("action", "up");
+        div.appendChild(calendarUpButton);
+        this._shadow.appendChild(div);
+        let daysOfWeek = document.createElement("cap-days-of-week");
+        this._shadow.appendChild(daysOfWeek);
+        let calendar = document.createElement("cap-grid-calendar");
+        this._shadow.appendChild(calendar);
+        let eventDate = document.createElement("cap-event-date");
+        this._shadow.appendChild(eventDate);
+    }
+}
+customElements.define("cap-calendar", Calendar)
