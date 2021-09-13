91a8892ae74d3aa6c29de0095883227c62519d12
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-13 14:04:10 +0200

    Grid-calendar finished

--- src/components/gridCalendar/gridCalendar.css.js
+++ src/components/gridCalendar/gridCalendar.css.js
@@ -1,15 +1,24 @@
 const sheet = new CSSStyleSheet();
 sheet.replace(`:host{
-                    border-bottom: 0.05em solid var(--border-color);
+                    border-bottom: 0.05em solid rgb(70,70,70);
                     color: white;
                     background-color: rgb(70, 70, 70);
                     font-family: sans-serif;
                     display: grid;
-                    padding: 1em 2em;
+                    padding: 1rem 1.7rem;
                     gap: 0.8em 2em;
                     grid-template-columns: repeat(7,1em);
                     text-align: center;
                     cursor: default;
+                }
+                :host > div{
+                    height: 1.7em;
+                    width: 1.7em;
+                    justify-content: space-around;
+                    display: flex;
+                    border-width: 2px;
+                    border-style: solid;
+                    border-color: transparent;
                 }
                 :host .isNotMonth{
                     color: grey;
@@ -17,8 +26,8 @@ sheet.replace(`:host{
                 :host .isToday{
                     background-color:rgb(127, 178, 240);
                 }
-                :host .selected{
-                    border-color: white;
+                :host .selected {
+                    border-color: rgb(127, 178, 240);
                 }`
     );
 export default sheet;
